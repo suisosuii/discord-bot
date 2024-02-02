bb943fe9a11860fcbd2bd83e345a98131cd4bd95
suisosuii <hayatokomiyama@icloud.com>
2024-02-02 23:29:06 +0900

    auto 復活

---
+++ auto.py
@@ -0,0 +1,26 @@
+import asyncio
+import aiohttp
+import time
+
+async def access_website(session, url):
+    try:
+        async with session.get(url) as response:
+            return await response.text()
+    except Exception as e:
+        print(f"An error occurred while accessing {url}: {e}")
+        return None
+
+async def schedule_access(url, interval):
+    async with aiohttp.ClientSession() as session:
+        while True:
+            print(f"Accessing {url}...")
+            content = await access_website(session, url)
+            if content is not None:
+                print(f"Accessed {url}.")
+            await asyncio.sleep(interval)
+
+url = "https://dis-bot-tnme.onrender.com/"  # アクセスしたいウェブサイトのURL
+interval = 600  #10分 
+
+async def auto():
+    await schedule_access(url, interval)
--- main.py
+++ main.py
@@ -3,14 +3,10 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 from keep_alive import keep_alive
-# import asyncio
+from auto import auto
 
-
-#from cogs.music import music_cog
-#from cogs.util import util_cog
 from cogs.reaction import rec_cog
 from cogs.mention import men_cog
-
 
 class MyBot(commands.Bot):
     def __init__(self):
@@ -29,14 +25,12 @@ class MyBot(commands.Bot):
             except discord.errors.Forbidden:
                 print(f"Guild {guild_id} is not found.")
 
-
 load_dotenv()
 
 bot = MyBot()
 
-
 bot.add_cog(rec_cog(bot))
 bot.add_cog(men_cog(bot))
 keep_alive()
+bot.loop.create_task(auto())  # auto関数をバックグラウンドタスクとして追加
 bot.run(os.environ["TOKEN"] or "")
-
