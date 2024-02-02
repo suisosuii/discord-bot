1a2f4a9a469070cdd11522a4bbd71f13c013ca45
suisosuii <hayatokomiyama@icloud.com>
2024-02-02 23:08:08 +0900

    Revert "log追加"
    This reverts commit 1991a2930ed4ebb5f8a8cbdd7f90034d337cb262.

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,13 @@
 from flask import Flask
 from threading import Thread
-import logging
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    app.logger.info("I'm alive")
     return "I'm alive"
 
 def run():
-    logging.basicConfig(filename='server.log', level=logging.INFO)
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
